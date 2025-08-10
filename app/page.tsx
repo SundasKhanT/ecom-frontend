c6c494f96b7cd58e868b7c6371af369312e11ed6
Sundas <sundaskhanhere@gmail.com>
2025-08-10 23:50:48 +0500

    initial setup prettier, tailwindcss, and global file

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full font-bold h-screen flex flex-col gap-2 items-center justify-center">
+      Welcome to HR software
+    </div>
+  );
+}
